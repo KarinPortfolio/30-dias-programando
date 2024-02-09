732a52528a5a994bbac64dc8abd0eddcf056ff2d
Karin <karin.abe@gmail.com>
2024-02-09 19:43:31 -0300

    Desafio07

--- Desafio_BackEnd/Desafio07/Main.java
+++ Desafio_BackEnd/Desafio07/Main.java
@@ -1,14 +1,12 @@
-/* Calcular quantos azulejos serão necessários para cobrir uma parede de 2m de altura por 3m de largura, considerando que cada azulejo mede 25cm x 25cm
-*/
+/* Elaborar um programa que apresente o somatório dos números na faixa de 1 até 500. */
+public class Main {
+    public static void main(String[] args) {
+        int somatorio = 0;
 
+        for (int i = 1; i <= 500; i++) {
+              somatorio += i; // Adicionando cada número à soma
+        }
 
-public class Main{
-  public static void main(String[] args){
-    double altPar = 2.0;
-    double larPar = 3.0;
-    double altAz = 0.25;
-    double larAz = 0.25;
-
-   double quantidade = (altPar * larPar) / (altAz * larAz);
-System.out.println("A quantidade de azulejos necessária é " + quantidade);
+        System.out.println("O somatório de 1 a 500 é: " + somatorio);
+    }
 }
